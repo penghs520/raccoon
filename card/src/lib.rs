3a2554d20783f1e5e0a49cd38b0fd9a68aeb1ccd
penghs <penghs@agilean.cn>
2024-09-30 18:42:01 +0800

    gitignore

---
+++ card/src/lib.rs
@@ -0,0 +1,22 @@
+pub mod card;
+pub mod store;
+pub mod query;
+mod graph;
+
+#[cfg(test)]
+mod tests {
+    use crate::card::{Card, CardState};
+
+    #[test]
+    #[test]
+    fn test_new_card() {
+        let card = Card::new("10001".to_string(), "卡片01".to_string(), "1", "1");
+        println!("{:?}", card);
+        assert_eq!(card.code(), "10001");
+        assert_eq!(card.type_id(), "1");
+        assert_eq!(card.tenant_id(), "1");
+        assert_eq!(card.fields().len(), 0);
+        assert_eq!(card.name(), "卡片01");
+        assert_eq!(card.state(), &CardState::Active);
+    }
+}
